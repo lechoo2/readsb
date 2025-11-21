1981728c07c5c1d1530c6d276b0000d16499a90b
lechoo2 <les65fm@gmail.com>
2025-11-21 13:19:03 +0100

    Fix declaration of ais_charset array

--- ais_charset.c
+++ ais_charset.c
@@ -23,4 +23,4 @@
 
 #include "ais_charset.h"
 
-char ais_charset[64] = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_ !\"#$%&'()*+,-./0123456789:;<=>?";
+char ais_charset[] = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_ !\"#$%&'()*+,-./0123456789:;<=>?";
